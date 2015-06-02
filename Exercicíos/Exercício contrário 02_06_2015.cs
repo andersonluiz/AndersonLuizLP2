9af52af98388f861a5dc028122390d361b44a1e5
Anderson Luiz De Melo Viana <And174_br@yahoo.com.br>
2015-06-02 14:52:32 -0300

    Exercicio Contratrio

---
+++ Exercicíos/Exercício contrário 02_06_2015.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            Console.WriteLine("Escreva um texto :");
+            frase = Convert.ToString(Console.ReadLine());
+            char a;
+            for (int i = frase.Length - 1; i >= 0; i--)
+            {
+                a = frase[i];
+                Console.Write(a);
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
