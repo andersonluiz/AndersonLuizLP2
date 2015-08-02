56af1ba3bbc4962d41227dd251d737cb338ca8c1
Anderson Luiz De Melo Viana <and174_br@yahoo.com.br>
2015-08-02 05:52:06 -0300

    Professor por algum motivo o exercicio 9 que fiz com classe em aula  estava vazio então eu criei outro mas deixei aquele para o senhor ver que eu não mudei nada apenas por algum motivo obscuro ele está vazio.

---
+++ Exercicíos/Exerc Funcionario 23062015/Exerc Funcionario 23062015/Funcionario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_Funcionario_23062015
+{
+    class Funcionario
+    {
+        public string Nome;
+        public string Cpf;
+        public DateTime DataNascimento;
+        public DateTime DataAdmissao;
+        public double salario;
+
+        public double Aumento()
+        {
+            double aumento = 0;
+            if (DataAdmissao.Year <= 2014)
+            {
+                aumento += 423.45;
+            }
+
+            else if (DataAdmissao.Year >= 2010 && DataAdmissao.Year < 2013)
+            {
+                aumento = (salario * 9.84) / 100;
+            }
+
+            else if (DataAdmissao.Year >= 2000 && DataAdmissao.Year < 2009)
+            {
+                aumento = (salario * 16.4) / 100;
+            }
+
+            else if (DataAdmissao.Year <= 1999)
+            {
+                aumento = (salario * 22.6) / 100;
+            }
+
+            return aumento;
+        }
+
+    }
+}
