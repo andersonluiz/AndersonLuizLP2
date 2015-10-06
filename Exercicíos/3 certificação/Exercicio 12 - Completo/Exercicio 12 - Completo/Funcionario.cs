03a3addff3a2ce1308218c8c928e24da64fbf78a
Anderson Luiz De Melo Viana <and174_br@yahoo.com.br>
2015-10-06 01:44:43 -0300

    Exercicio 12 - Completo.csproj

---
+++ Exercicíos/3 certificação/Exercicio 12 - Completo/Exercicio 12 - Completo/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_12___Completo
+{
+    class Funcionario
+    {
+        public string Nome;
+        public string CPF;
+        public DateTime DataDeNascimento;
+        public string Matricula;
+        public double Salario { get; set; }
+        public double ValeAlimentacao()
+        {
+            double vale = Salario * 0.08;
+
+            if (vale > 750)
+            {
+                return 750;
+            }
+            return vale;
+        }
+    }
+}
---
+++ Exercicíos/3 certificação/Exercicio 12 - Completo/Exercicio 12 - Completo/Terceirizado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_12___Completo
+{
+    class Terceirizado
+    {
+        public string Nome;
+        public string CPF;
+        public DateTime DataDeNascimento;
+        public string Matricula;
+        public string CNPJ;
+        
+    }
+}
